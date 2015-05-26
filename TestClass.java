1a19ebabae28ad8ee12d71d5ec9274a2ce3bfb93
Stevo5o <stevo.joc@gmail.com>
2015-05-26 12:43:44 +0100

    Explicit import statements

---
+++ TestClass.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.text.*;
+import java.util.*;
+import java.util.logging.*;
+
+public class TestClass {
+   public static void main(String[] args) throws IOException {
+
+    /* ensure directory has been created */
+    new File("logs").mkdir();
+
+    /* get the date to be used in the filename */
+    DateFormat df = new SimpleDateFormat("yyyyMMddhhmmss");
+    Date now = new Date();
+    String date = df.format(now);
+
+    /* set up the filename in the logs directory  */
+    String logFileName = "logs\\testlog-" + date + ".txt";
+
+    // set up logger
+    FileHandler myFileHandler = new FileHandler(logFileName);
+    myFileHandler.setFormatter(new SimpleFormatter());
+    Logger ocajLogger = Logger.getLogger("OCAJ Logger");
+    ocajLogger.setLevel(Level.ALL);
+    ocajLogger.addHandler(myFileHandler);
+
+    // log message
+    ocajLogger.info("\nThis is a logged information message.");
+
+    // close file
+    myFileHandler.close();
+
+  }
+}
