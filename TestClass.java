1413b3ab391300e1919e9eb06cde39085969c732
Stevo5o <stevo.joc@gmail.com>
2015-05-26 13:12:59 +0100

    Replace 4 implicit with 9 explicit import statements

--- TestClass.java
+++ TestClass.java
@@ -1,20 +1,30 @@
-import java.io.*;
-import java.text.*;
-import java.util.*;
-import java.util.logging.*;
+// import java.io.*;
+import java.io.IOException;
+import java.io.File;
+// import java.text.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+// import java.util.*;
+import java.util.Date;
+// import java.util.logging.*;
+import java.util.logging.FileHandler;
+import java.util.logging.SimpleFormatter;
+import java.util.logging.Logger;
+import java.util.logging.Level;
+
 
 public class TestClass {
    public static void main(String[] args) throws IOException {
 
-    /* ensure directory has been created */
+    // ensure directory has been created
     new File("logs").mkdir();
 
-    /* get the date to be used in the filename */
+    // get the date to be used in the filename
     DateFormat df = new SimpleDateFormat("yyyyMMddhhmmss");
     Date now = new Date();
     String date = df.format(now);
 
-    /* set up the filename in the logs directory  */
+    // set up the filename in the logs directory
     String logFileName = "logs\\testlog-" + date + ".txt";
 
     // set up logger
