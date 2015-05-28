1a39e58404db464977675f47d0dcc883b06e847e
Stevo5o <stevo.joc@gmail.com>
2015-05-28 13:14:31 +0100

    Basic class

---
+++ Lesson01/App.java
@@ -0,0 +1,27 @@
+class Person {
+  // class template for creating objects
+  // instance variables (data or "state")
+  String name;
+  int age;
+
+  // class can contain:
+  // 1. data
+  // 2. subroutines (methods)
+
+}
+
+public class App {
+
+  public static void main(String[] args) {
+
+    Person person1 = new Person();
+    person1.name = "Brendan Behan";
+    person1.age = 41;
+
+    Person person2 = new Person();
+    person2.name = "Sinead O'Connor";
+    person2.age = 48;
+
+    System.out.println(person1.name + " & " + person2.name);
+  }
+}
