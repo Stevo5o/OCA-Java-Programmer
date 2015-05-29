389e35c15da86a17835f28f2c3b38513c53606ee
Stevo5o <stevo.joc@gmail.com>
2015-05-29 14:46:38 +0100

    switch statement

--- Lesson02/ConditionalStatements.java
+++ Lesson02/ConditionalStatements.java
@@ -9,5 +9,48 @@ public class ConditionalStatements {
       inFishingTrip = true;
       System.out.println("Going fishing!");
     }
+
+    boolean b;
+    boolean bValue = (b = true); // evaluates to true
+
+    if (bValue) {
+      System.out.println("TRUE");
+    }
+    else {
+      System.out.println("FALSE");
+    }
+    if (bValue = false) {
+      System.out.println("TRUE");
+    }
+    else {
+      System.out.println("FALSE");
+    }
+    if (bValue == false) {
+      System.out.println("TRUE");
+    }
+    else {
+      System.out.println("FALSE");
+    }
+
+    public string generateRandomFish(){
+      String randomFish;
+      Random randomObject = new Random();
+      int randomNumber = randomObject.nextInt(4);
+
+      switch (randomNumber) {
+        case 0:
+        randomFish = "Blue Fin Tuna";
+        break;
+        case 1:
+        randomFish = "Sea Bass";
+        break;
+        randomFish = "Machrel";
+        break;
+        default:
+        randomFish = "Unknown Fish Type";
+        break;
+      }
+      return randomFish;
+    }
   }
 }
