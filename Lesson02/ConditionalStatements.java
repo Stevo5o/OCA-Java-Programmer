9720888dc4dcc3eef87f3a30c7e65e8f353be32b
Stevo5o <stevo.joc@gmail.com>
2015-05-30 20:59:30 +0100

    if then else

--- Lesson02/ConditionalStatements.java
+++ Lesson02/ConditionalStatements.java
@@ -32,25 +32,38 @@ public class ConditionalStatements {
       System.out.println("FALSE");
     }
 
-    public string generateRandomFish(){
-      String randomFish;
-      Random randomObject = new Random();
-      int randomNumber = randomObject.nextInt(4);
+    // public string generateRandomFish() {
+    //   String randomFish;
+    //   Random randomObject = new Random();
+    //   int randomNumber = randomObject.nextInt(4);
+    //
+    //   switch (randomNumber) {
+    //     case 0:
+    //     randomFish = "Blue Fin Tuna";
+    //     break;
+    //     case 1:
+    //     randomFish = "Sea Bass";
+    //     break;
+    //     randomFish = "Machrel";
+    //     break;
+    //     default:
+    //     randomFish = "Unknown Fish Type";
+    //     break;
+    //   }
+    //   return randomFish;
+    // }
 
-      switch (randomNumber) {
-        case 0:
-        randomFish = "Blue Fin Tuna";
-        break;
-        case 1:
-        randomFish = "Sea Bass";
-        break;
-        randomFish = "Machrel";
-        break;
-        default:
-        randomFish = "Unknown Fish Type";
-        break;
-      }
-      return randomFish;
+    int pyramidSinker = 3;
+    System.out.print("A pyramid sinker that weighs " + pyramidSinker
+    + "ounces is ");
+    if (pyramidSinker == 2) {
+      System.out.print("used for a slow moving tide. ");
+    }
+    else if (pyramidSinker == 3) {
+      System.out.print("used for a moderate  moving tide. ");
+    }
+    else if (pyramidSinker == 4) {
+      System.out.print("used for a fast moving tide. ");
     }
   }
 }
