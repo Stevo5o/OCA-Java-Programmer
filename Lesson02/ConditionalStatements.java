6415aaa3fe23c0409bac5f5fd325576ce6dccc36
Stevo5o <stevo.joc@gmail.com>
2015-06-01 14:42:49 +0100

    Switch with enum

--- Lesson02/ConditionalStatements.java
+++ Lesson02/ConditionalStatements.java
@@ -2,68 +2,26 @@ public class ConditionalStatements {
 
   public static void main(String[] args) {
 
-    int isFishermen = 2;
-    boolean inFishingTrip = false;
+    ClamBait bait = ClamBait.SALTED;
 
-    if (isFishermen > 1) {
-      inFishingTrip = true;
-      System.out.println("Going fishing!");
-    }
-
-    boolean b;
-    boolean bValue = (b = true); // evaluates to true
-
-    if (bValue) {
-      System.out.println("TRUE");
-    }
-    else {
-      System.out.println("FALSE");
-    }
-    if (bValue = false) {
-      System.out.println("TRUE");
-    }
-    else {
-      System.out.println("FALSE");
-    }
-    if (bValue == false) {
-      System.out.println("TRUE");
-    }
-    else {
-      System.out.println("FALSE");
-    }
-
-    // public string generateRandomFish() {
-    //   String randomFish;
-    //   Random randomObject = new Random();
-    //   int randomNumber = randomObject.nextInt(4);
-    //
-    //   switch (randomNumber) {
-    //     case 0:
-    //     randomFish = "Blue Fin Tuna";
-    //     break;
-    //     case 1:
-    //     randomFish = "Sea Bass";
-    //     break;
-    //     randomFish = "Machrel";
-    //     break;
-    //     default:
-    //     randomFish = "Unknown Fish Type";
-    //     break;
-    //   }
-    //   return randomFish;
-    // }
-
-    int pyramidSinker = 3;
-    System.out.print("A pyramid sinker that weighs " + pyramidSinker
-    + "ounces is ");
-    if (pyramidSinker == 2) {
-      System.out.print("used for a slow moving tide. ");
-    }
-    else if (pyramidSinker == 3) {
-      System.out.print("used for a moderate  moving tide. ");
-    }
-    else if (pyramidSinker == 4) {
-      System.out.print("used for a fast moving tide. ");
+    switch (bait)
+    {
+      default:
+          System.out.println("No bait");
+          break;
+        case FRESH:
+          System.out.println("Fresh clams");
+          break;
+        case SALTED:
+          System.out.println("Salted clams");
+          break;
+        case ARTIFICAL:
+          System.out.println("Artifical clams");
+          break;
     }
   }
+}
+enum ClamBait
+{
+  FRESH, SALTED, ARTIFICAL
 }
