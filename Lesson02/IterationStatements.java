4df1f050ad65ce26a29d6ba29f9869b914032c97
Stevo5o <stevo.joc@gmail.com>
2015-06-03 16:54:54 +0100

    Basic for loop

---
+++ Lesson02/IterationStatements.java
@@ -0,0 +1,12 @@
+public class IterationStatements {
+
+ public static void main(String[] args) {
+
+   int m;
+   for (m = 1; m < 5; m++) {
+     System.out.print("Marker " + m + ", ");
+   }
+
+   System.out.print("Last Marker " + m + "\n");
+ }
+}
