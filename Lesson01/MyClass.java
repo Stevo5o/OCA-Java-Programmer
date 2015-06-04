856f4bd038c206758e590358fd42bf20187413fe
Stevo5o <stevo.joc@gmail.com>
2015-06-04 15:34:02 +0100

    Enter cli args and import static

---
+++ Lesson01/MyClass.java
@@ -0,0 +1,9 @@
+import static java.lang.Integer.*;
+
+public class MyClass {
+  public static void main(String[] args) {
+    // System.out.println(args[0] + " " + args[1]);
+    System.out.println(MAX_VALUE);
+    System.out.println(toHexString(42));
+  }
+}
