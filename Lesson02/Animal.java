7326b6f445fd1f56683f7452806c559dc370f2c4
Stevo5o <stevo.joc@gmail.com>
2015-06-11 13:37:54 +0100

    HAS-A method

--- Lesson02/Animal.java
+++ Lesson02/Animal.java
@@ -1,4 +1,13 @@
 public class Animal { }
 public class Lizard extends Animal {
-	private Tank myTank;
+	private Terrarium myTerrarium = new Terrarium();
+	public void clean(Materials sponge) {
+		myTerrarium.clean(sponge); // delegate clean behaviour to the
+							       // Terrarium object			
+	}
+}
+public class Halter {
+	public void clean(Materials aSponge) {
+		// do the clean work here
+	}
 }
