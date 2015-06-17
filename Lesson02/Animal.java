8d5c9516a49d7ce623854cf81246a5fb47bdaf38
Stevo5o <stevo.joc@gmail.com>
2015-06-17 19:52:19 +0100

    Casting

--- Lesson02/Animal.java
+++ Lesson02/Animal.java
@@ -1,4 +1,24 @@
 class Animal { 
+	void makeNoise() {System.ot.println("generic noise"); }
+}
+class Dog extends Animal {
+	void makeNoise() {
+		System.out.println("bark");
+	}
+	void playDead() {
+		System.out.println("roll over");
+	}
+}
+
+class CastTest2 {
+	public static void main(String[] args) {
+	Animal[] a = {new Animal(), new Dog(), new Animal()};
+	for(Animal animal : a ) {
+		animal.makeNoise();
+		if(animal instanceof Dog) {
+			animal.playDead();
+		}
+	}
 	public void printYourself() {
 		System.out.println("Print Yourself method");
 	}
