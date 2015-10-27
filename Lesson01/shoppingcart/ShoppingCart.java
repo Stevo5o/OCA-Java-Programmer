d0fab13feb11f766c87d4490e6a3cbda8b1c3821
Stevo5o <stevo.joc@gmail.com>
2015-10-27 17:08:53 +0000

    Hello W & ShoppingCart :cactus:

---
+++ Lesson01/shoppingcart/ShoppingCart.java
@@ -0,0 +1,12 @@
+public class ShoppingCart 
+{
+	public static void main(String[] args) 
+	{
+		String custName = "Stephen O'Connor";
+		String itemDesc = "shirt";
+		String message;
+		
+		message = custName + " wants to purcahse a " + itemDesc;
+		System.out.println(message);
+	}	
+}
