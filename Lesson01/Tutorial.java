c36a87b45f89789693a7a89482c2681ac4bf6d50
Stevo5o <stevo.joc@gmail.com>
2015-10-28 18:05:38 +0000

    Declaring and Calling Methods :cake:

---
+++ Lesson01/Tutorial.java
@@ -0,0 +1,18 @@
+public class Tutorial
+{
+	public static void main(String[] args) 
+	{
+		correct();
+		incorrect();
+	}
+	
+	public static void correct() 
+	{
+		System.out.println("Correct, great job!");
+	}
+	
+	public static void  incorrect() 
+	{
+		System.out.print("Incorrect, try again.\n");
+	}
+}
