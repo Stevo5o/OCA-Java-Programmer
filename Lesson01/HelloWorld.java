026ad19630c58456ba9866cf22e54d5c91402ad6
Stevo5o <stevo.joc@gmail.com>
2015-11-06 15:29:56 +0000

    Basic While loop :watermelon:

--- Lesson01/HelloWorld.java
+++ Lesson01/HelloWorld.java
@@ -3,5 +3,14 @@ public class HelloWorld
 	public static void main(String[] args) 
 	{
 		System.out.println("Hello World!");
-	}	
+		
+		// example of a while loop
+		int i = 1;
+		while ( i <= 10)
+		{
+			System.out.println("Perfect square #" + i +
+				" is: " + i + "*" + i + "=" + i*i);
+			i++;
+		}
+	}
 }
