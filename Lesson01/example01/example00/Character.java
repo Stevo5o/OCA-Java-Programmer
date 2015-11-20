b3c8ce954ca96625bbaf16d54626d89d21271d79
Stevo5o <stevo.joc@gmail.com>
2015-11-20 14:15:04 +0000

    CLI Java and packages :shipit:

---
+++ Lesson01/example01/example00/Character.java
@@ -0,0 +1,27 @@
+public class Character 
+{
+    String name;
+
+    public Character()
+    {
+    }
+    public Character(String name) 
+    {
+        this.name = name;
+    }
+    
+    public String getName() 
+    {
+        return name;
+    }
+    
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+    
+    public String toString()
+    {
+        return name;
+    }
+}
---
+++ Lesson01/example01/example00/CharacterMain.java
@@ -0,0 +1,10 @@
+public class CharacterMain
+{
+    public static void main(String[] args)
+    {
+        Character a = new Character("Ste");
+        Character b = new Character("Bar");
+        
+        System.out.println("We have 2 characters: " + a + " and " + b);
+    }
+}
