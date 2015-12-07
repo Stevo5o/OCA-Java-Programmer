f932548a151656130b3a822ced76800c4ffae74a
theWhiteFox <ste@thewhitefox.ninja>
2015-12-07 14:52:59 +0000

    Overriding a method :coffee:

---
+++ Lesson03/ClassA.java
@@ -0,0 +1,13 @@
+/* Overriding a method */
+public class ClassA
+{
+  public void sayHello()
+  {
+    System.out.print("Hello from A");
+  }
+
+  public void sayGoodbye()
+  {
+    System.out.print("Goodbye");
+  }
+}
---
+++ Lesson03/ClassB.java
@@ -0,0 +1,11 @@
+public class ClassB extends ClassA
+{
+  public static void main(String[] args)
+  {
+    @Override
+    public void sayHello()
+    {
+      System.out.print("Hello from B");
+    }
+  }
+}
