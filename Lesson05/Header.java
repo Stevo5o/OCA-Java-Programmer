889d3353c066e704ebc0d5958bd231681c7578fa
theWhiteFox <ste@thewhitefox.ninja>
2016-01-04 13:53:10 +0000

    Main method :anger:

--- Lesson05/Header.java
+++ Lesson05/Header.java
@@ -32,4 +32,18 @@ final public class Header
 	{
 		return "Title: " + title + "\nVersion: " + version + "\nDate: " + date;
 	}
+	
+	public static void main(String[] args)
+	{
+		Date date = new Date();
+		Header header = new Header("Ste", 3, date);		
+		System.out.println(header.toString());
+	}
 }
+
+/* 
+ * Output
+ * Title: Ste
+ * Version: 3
+ * Date: Mon Jan 04 13:49:04 GMT 2016
+*/
