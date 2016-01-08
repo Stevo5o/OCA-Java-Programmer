d46ae9ebf255d088c74b6f66b5ae80546c12aee6
theWhiteFox <ste@thewhitefox.ninja>
2016-01-08 13:50:57 +0000

    OOP :grin:

---
+++ Lesson05/SecondClass.java
@@ -0,0 +1,8 @@
+public class SecondClass
+{
+	void add(int x, int y)
+	{
+		int answer = x + y;
+		System.out.println(answer);	
+	}
+}
---
+++ Lesson05/Tutorial.java
@@ -0,0 +1,8 @@
+public class Tutorial
+{
+	public static void main(String [] args)
+	{
+		SecondClass sc = new SecondClass();
+		sc.add(10, 30); 
+	}
+}
