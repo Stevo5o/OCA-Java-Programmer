04d1e044d69ad165baafd8ac27678dbe1f07b7e0
theWhiteFox <ste@thewhitefox.ninja>
2016-02-08 20:40:03 +0000

    Clean and new Q&A :finnadie:

---
+++ Lesson01/q&a/QA2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class QA2
+{
+	public static void main(String [] args)
+	{
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Enter name: ");		
+		String username = scanner.next();
+		System.out.println("Hi " + username + " welcome back!");
+		
+	}
+}
