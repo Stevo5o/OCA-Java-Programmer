1bcf6151dd53155eb2c295b96fb572e7e8d4b42a
theWhiteFox <ste@thewhitefox.ninja>
2016-02-09 13:31:40 +0000

    Q & A :man:

--- Lesson01/q&a/QA2.java
+++ Lesson01/q&a/QA2.java
@@ -1,14 +1,17 @@
-import java.util.Scanner;
-
 class QA2
 {
 	public static void main(String [] args)
-	{
-		Scanner scanner = new Scanner(System.in);
+	{		
+		for(String i: args)
+		{
+			System.out.println(i);
+		}
+		// int firstNum = args[0];
 		
-		System.out.println("Enter name: ");		
-		String username = scanner.next();
-		System.out.println("Hi " + username + " welcome back!");
+		// System.out.println(firstNum);
+		// System.out.println(args[1]);
 		
+		// ArrayList list = new ArrayList();
+		// list.add(firstNum);		
 	}
 }
