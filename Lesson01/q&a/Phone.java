694b2c85d28683cedba545486c655e45506e6a65
theWhiteFox <ste@thewhitefox.ninja>
2016-02-12 14:31:57 +0000

    QA11 StringBuilder :lollipop:

--- Lesson01/q&a/Phone.java
+++ Lesson01/q&a/Phone.java
@@ -136,7 +136,7 @@ class Phone implements Communicatable
 	
 	public void streamVideo()
 	{
-		
+		System.out.println("Streaming");
 	}
 	
 	public static void main(String [] args)
@@ -149,10 +149,15 @@ class Phone implements Communicatable
 		phones.add(landLine2000);
 		phones.add(g200);
 		
+		StringBuilder sb = new StringBuilder();
+		
 		for (Phone phone : phones) 
-		{			
-			System.out.println(phone);	    
+		{		
+			sb.append(phone);
+    		sb.append(",");	
    		}	
+		   
+		System.out.println(sb);
 		
 		System.out.println();		
 		
@@ -162,19 +167,49 @@ class Phone implements Communicatable
 		landLine2000.sendText("Hi very warm!","0874546432");
 		landLine2000.receiveText("Lucky you!","0864545454");
 		landLine2000.recharge(true);
+		
+		System.out.println();
+		
+		g200.makeCall("0874646372");
+		g200.receiveCall("0864546342");
+		g200.hangUp();
+		g200.sendText("Hi very warm!","0874546432");
+		g200.receiveText("Lucky you!","0864545454");
+		g200.recharge(true);
+		g200.streamVideo();
 	}
 }
 /*
 
- Name: G7
- No of pixels: 960
- Width: 9.3
- Height: 15.4
- Weight: 32.0
+ Name: LandLine 2000
+ No of pixels: 400
+ Width: 5.6
+ Height: 8.5
+ Weight: 80.5
  Powered On: true
  Rechargng: false
+
+ Name: G200
+ No of pixels: 510
+ Width: 4.5
+ Height: 8.6
+ Weight: 80.5
+ Powered On: true
+ Rechargng: false
+
+0874646372
+0864546342
+Hang up
+Hi very warm!0874546432
+Lucky you!0864545454
+true
+
+0874646372
+0864546342
+Hang up
+Hi very warm!0874546432
+Lucky you!0864545454
+true
+Streaming
  
 */
-
-
-// streamVideo()***
