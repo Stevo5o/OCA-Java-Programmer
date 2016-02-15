826e57514fa9b1577df6c8ed63b4707dd917089c
theWhiteFox <ste@thewhitefox.ninja>
2016-02-15 12:39:10 +0000

    add if :boom:

--- Lesson01/q&a/Phone.java
+++ Lesson01/q&a/Phone.java
@@ -100,30 +100,43 @@ class Phone implements Communicatable
 	{
 		System.out.println(noToDial);
 	}
+	
 	public void receiveCall (String incomingPhoneNo)
 	{
 		System.out.println(incomingPhoneNo);
 	}
+	
 	public void sendText (String messageToSend,String noToText)
 	{
-		System.out.println(messageToSend + noToText);
+		System.out.print(" \n Sending text: " + messageToSend + noToText);
 	}
+	
 	public void receiveText (String message, String incomingPhoneNo)
 	{
-		System.out.println(message + incomingPhoneNo);
+		System.out.print(" \n Incoming text: " + message + incomingPhoneNo);
 	}
+	
 	public void recharge(boolean status)
 	{
-		System.out.println(status);
+		if(status == true)
+		{
+			System.out.println(" is currently recharging");
+		}
+		
+		else 
+		{
+			System.out.println(" is not currently recharging");
+		}
+		
 	}
 	public void hangUp()
 	{
-		System.out.println("Hang up");
+		System.out.print(" Terminating a phone call on a ");
 	}
-	
+		
 	public void streamVideo()
 	{
-		System.out.print(" Curtenly Streaming on ");
+		System.out.print(" Currently Streaming on ");
 	}
 	
 	@Override
@@ -134,8 +147,8 @@ class Phone implements Communicatable
 				"\n Width: " + getWidth() +
 				"\n Height: " + getHeight() +
 				"\n Weight: " + getWeight() +
-				"\n Powered On: " + getIsPoweredOn() +
-				"\n Recharging: " + getIsRecharging() 
+				"\n Powered On?: " + getIsPoweredOn() +
+				"\n Recharging?: " + getIsRecharging() 
 		);
 	}
 	
@@ -150,23 +163,40 @@ class Phone implements Communicatable
 		phones.add(landLine2000);
 		phones.add(g200);
 		
-		int index = 0;
 		for (Phone phone : phones) 
 		{	
-			System.out.println(phone + " at index:" + (index++));
+			System.out.println(phone);
 			System.out.println();
-			phone.makeCall(" 0874646372");
-			phone.receiveCall(" 0864546342");
-			phone.hangUp();
-			phone.sendText(" Hi very warm!","0874546432");
-			phone.receiveText(" Lucky you!","0864545454");
-			phone.recharge(true);
+			
+			if(phone.getName().equals("LandLine 2000"))
+			{				
+				landLine2000.makeCall(" Dialling number 0874646372 on a LandLine2000 phone.");
+				landLine2000.receiveCall(" Incoming call from 0864546342 on a LandLine2000 phone.");
+				landLine2000.hangUp();
+				System.out.print(landLine2000.getName() + " phone");
+				landLine2000.sendText("Hi, very warm!"," to phone number 0874546432 from a ");
+				System.out.print(landLine2000.getName() + " phone");
+				landLine2000.receiveText("Lucky you!"," recieved from 0864545454 from a");
+				System.out.print(" " + landLine2000.getName() + " phone");
+				System.out.print(" \n " + landLine2000.getName() + " phone");
+				landLine2000.recharge(true);
+			}
 			
 			//check whether the name is G200 or not.
 			if(phone.getName().equals("G200"))
-			{				
-				phone.streamVideo();
-				System.out.print(phone.getName());
+			{	
+				g200.makeCall(" Dialling number 0874646372 on a G200 phone.");
+				g200.receiveCall(" Incoming call from 0864546342 on a G200 phone.");
+				g200.hangUp();
+				System.out.print(g200.getName() + " phone");
+				g200.sendText("Hi, very warm!"," to phone number 0874546432 from a ");
+				System.out.print(g200.getName() + " phone");
+				g200.receiveText("Lucky you!"," recieved from 0864545454 from a");
+				System.out.print(" " + g200.getName() + " phone");
+				System.out.print(" \n " + g200.getName() + " phone");
+				g200.recharge(true);			
+				g200.streamVideo();
+				System.out.print(g200.getName());
 			}
    		}
 		
@@ -175,36 +205,34 @@ class Phone implements Communicatable
 	}
 }
 /*
-
-  Name: LandLine 2000
+ Name: LandLine 2000
  No of pixels: 400
  Width: 5.6
  Height: 8.5
  Weight: 80.5
- Powered On: true
- Recharging: false at index:0
+ Powered On?: true
+ Recharging?: false
 
- 0874646372
- 0864546342
-Hang up
- Hi very warm!0874546432
- Lucky you!0864545454
-true
+ Dialling number 0874646372 on a LandLine2000 phone.
+ Incoming call from 0864546342 on a LandLine2000 phone.
+ Terminating a phone call on a LandLine 2000 phone 
+ Sending text: Hi, very warm! to phone number 0874546432 from a LandLine 2000 phone 
+ Incoming text: Lucky you! recieved from 0864545454 from a LandLine 2000 phone 
+ LandLine 2000 phone is currently recharging
 
  Name: G200
  No of pixels: 510
  Width: 4.5
  Height: 8.6
  Weight: 80.5
- Powered On: true
- Recharging: false at index:1
+ Powered On?: true
+ Recharging?: false
 
- 0874646372
- 0864546342
-Hang up
- Hi very warm!0874546432
- Lucky you!0864545454
-true
- Curtenly Streaming on G200
- 
+ Dialling number 0874646372 on a G200 phone.
+ Incoming call from 0864546342 on a G200 phone.
+ Terminating a phone call on a G200 phone 
+ Sending text: Hi, very warm! to phone number 0874546432 from a G200 phone 
+ Incoming text: Lucky you! recieved from 0864545454 from a G200 phone 
+ G200 phone is currently recharging
+ Currently Streaming on G200
 */
