63d0fb05ecc9e6b32e76332002cc5d163413e8e0
theWhiteFox <ste@thewhitefox.ninja>
2016-03-20 21:25:53 +0000

    :ear: String Object :moon:

--- Lesson03/StringTest.java
+++ Lesson03/StringTest.java
@@ -9,5 +9,19 @@ class StringTest
 		x = x + " Bean"; // now modify the object using the x reference
 		
 		System.out.println("y String = " + y);
+		
+		String s = "Fred";
+		String t = s;
+		s = s + " & Ted";
+		String e = s + " & Ste";
+		
+		System.out.println("String e: " + e + "\nString s: " + s + " \nString t: " + t.toUpperCase());
 	}
 }
+/*
+y String = Java
+y String = Java
+String e: Fred & Ted & Ste
+String s: Fred & Ted 
+String t: FRED
+*/
