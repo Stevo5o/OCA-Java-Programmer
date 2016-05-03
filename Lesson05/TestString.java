da1e5fabff8264895510f5271d4b03ba5febe527
theWhiteFox <ste@thewhitefox.ninja>
2016-05-03 12:36:04 +0100

    :angel: String methods :couple:

--- Lesson05/TestString.java
+++ Lesson05/TestString.java
@@ -35,12 +35,35 @@ class TestString
 		k += " ocean";
 		System.out.println(k);
 		
-		String m = "exit";
-		System.out.println(m.equalsIgnoreCase("EXIT"));
+		String m = "ExiT";
+		System.out.println(m.equalsIgnoreCase("exit"));
 		System.out.println(m.equalsIgnoreCase("xtie"));
 		
-		String n = "1234567";
-		System.out.println(n.length());		
+		String n = "0123456789";
+		System.out.println(n.length());	
+		
+		// length() method - String, length attribute - Array
+		String test = "test";
+		// System.out.println(test.length); error: cannot find symbol
+		String [] testArr = new String[3];
+		// System.out.println(testArr.length()); error: cannot find symbol
+		
+		// public String replace(char old, char new)
+		String p = "xoxoxo";
+		System.out.println("Replace x with X: " + p.replace('x', 'X'));
+		
+		// public String substring(int begin) and public String substring(int begin, int end)
+		System.out.println("Start at index 5 and return the rest of the String. " + n.substring(5));
+		System.out.println("Start at index 5 and return the characters up to and including the 8th position (index 7). " + n.substring(5, 8));
+		
+		System.out.println("EXIT lower case: " + m.toLowerCase());
+		System.out.println("String m to String: " + m.toString());
+		System.out.println("EXIT upper case: " + m.toUpperCase());
+		
+		String a = "  hi  ";
+		System.out.println(a);
+		System.out.println(a.trim() + "t");
+		
 	}
 }
 /*
@@ -53,4 +76,21 @@ Atlantic ocean
 true
 false
 7
+System.out.println(test.length);
+		               ^
+  symbol:   variable length
+  location: variable test of type String
+ 
+System.out.println(testArr.length()); 
+		                  ^
+  symbol:   method length()
+  location: variable testArr of type String[]
+Replace x with X: XoXoXo
+Start at index 5 and return the rest of the String. 56789
+Start at index 5 and return the characters up to and including the 8th position (index 7). 567
+EXIT lower case: exit
+String m to String: ExiT
+EXIT upper case: EXIT
+  hi  
+hit
 */
