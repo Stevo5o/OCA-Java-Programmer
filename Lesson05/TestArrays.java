9d4dfe2c39ee2b36b1ad51009999593d0f001174
theWhiteFox <ste@thewhitefox.ninja>
2016-05-07 23:00:42 +0100

    :dizzy: Perm missing element :musical_note:

--- Lesson05/TestArrays.java
+++ Lesson05/TestArrays.java
@@ -38,6 +38,14 @@ class TestArrays
 		
 		int[][] scores = {{5,2,4,7}, {9,2}, {3,4}}; 	
 		
-		System.out.println(Arrays.deepToString(scores));				
+		System.out.println(Arrays.deepToString(scores));
+		
+		int[] x = new int[5];
+		x[4] = 2;
+		// x[5] = 3; ArrayIndexOutOfBoundsException: 5 runtime
+		
+		int[] z = new int[2];
+		int y = -3;
+		// z[y] = 4; ArrayIndexOutOfBoundsException: -3 runtime
 	}
 }
