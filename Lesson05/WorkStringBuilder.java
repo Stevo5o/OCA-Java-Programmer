6279ad36945bc7065b8e5f1b04e26bb2662d748e
Stevo5o <stevo.joc@gmail.com>
2016-05-15 10:08:09 +0100

    :minidisc: Arrays basic :frowning:

--- Lesson05/WorkStringBuilder.java
+++ Lesson05/WorkStringBuilder.java
@@ -3,20 +3,32 @@ class WorkStringBuilder
     public static void main(String [] args)
     {
         StringBuilder sb = new StringBuilder("Teleportation successful.");
+        StringBuilder sb1 = new StringBuilder("Landing pad activated");
         System.out.println(sb);
+        System.out.println(sb1 + " " + sb1.length());
         System.out.println("Characters at postion 4: " + sb.charAt(4));
         System.out.println("Substring: " + sb.substring(2,6));
         System.out.println("Substring: " + sb.substring(5));      
-        System.out.println("Substring length: " + sb.capacity());
+        System.out.println("Length: " + sb.capacity());
         sb.append(" - Ludvig Maxis");
         System.out.println("Der Eisendrache: " + sb);
+        sb.setCharAt(24,'!');
+        sb.insert(0,"Der Eisendrache: ");
+        System.out.println(sb);
+        System.out.println("Capacity: " + sb.capacity());
+        System.out.println("Length: " + sb.length());
+        System.out.println(sb1.reverse());
     }
 }
 /*
 Teleportation successful.
+Landing pad activated 21
 Characters at postion 4: p
 Substring: lepo
 Substring: ortation successful.
-Substring length: 41
+Length: 41
 Der Eisendrache: Teleportation successful. - Ludvig Maxis
+Der Eisendrache: Teleportation successful! - Ludvig Maxis
+Capacity: 84
+Length: 57
 */
