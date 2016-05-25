1bb22c6f3d0215bd1fb3d7893f25a52c951490a3
theWhiteFox <ste@thewhitefox.ninja>
2016-05-25 19:15:36 +0100

    :lipstick: List ArrayList :hankey:

--- Lesson05/Cities.java
+++ Lesson05/Cities.java
@@ -4,6 +4,17 @@ class Cities
 {
 	public static void main(String [] args)
 	{
-		
+		List<String> c = new ArrayList<String>();
+		c.add("Oslo"); 
+		c.add("Paris"); 
+		c.add("Rome"); 
+		int index = c.indexOf("Paris");
+		System.out.println(c + " " + index);
+		c.add(index, "London");
+		System.out.println(c);
 	}
 }
+/*
+[Oslo, Paris, Rome] 1
+[Oslo, London, Paris, Rome]
+*/
